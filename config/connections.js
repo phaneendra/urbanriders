b582a66538f262fea362ca21d21c1132d3815e75
phaneendra <phaneendra.krishnareddy@gmail.com>
2014-04-30 00:33:29 +0800

    fix(bug fix): add mongo hq url for database connection

--- config/connections.js
+++ config/connections.js
@@ -50,11 +50,7 @@ module.exports.connections = {
     //
     mongodb: {
         adapter: 'sails-mongo',
-        host: 'localhost',
-        port: 27017,
-        user: '',
-        password: '',
-        database: 'urbanrunners'
+        url: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL
     },
 
     // PostgreSQL is another officially supported relational database.
